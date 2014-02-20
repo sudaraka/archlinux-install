aabf37e6438f836634448c7acc8611189bbd7374
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-02-21 03:53:41 +0530

    Adjusted some i3wm dependency packages

--- 052-install-gui.sh
+++ 052-install-gui.sh
@@ -49,8 +49,8 @@ case "$1" in
     "i3")
         # install i3
         yaourt -S --noconfirm \
-            i3 dmenu thunar thunar-archive-plugin ristretto tumbler orage \
-	        xfce4-notes-plugin xfce4-screenshooter conky
+            i3 i3bar-icons-git dmenu thunar thunar-archive-plugin ristretto \
+                tumbler orage xfce4-notes-plugin xfce4-screenshooter conky
         ;;
 esac;
 
