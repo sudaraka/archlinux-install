1459993876098afd8cf1d88602df6ee55308c3ba
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-06-28 02:50:27 +0530

    [bugfix] identifing ethernet adapter

--- 031-configure.sh
+++ 031-configure.sh
@@ -32,8 +32,8 @@ shift
 RFKILL=$1
 
 LAN_SERVER=`grep 'nameserver' /etc/resolv.conf|cut -d' ' -f2`;
-EN_DEV=`ip link|grep ': en\|eth'|cut -d':' -f2|tr -d ' '`;
-WL_DEV=`ip link|grep ': wl'|cut -d':' -f2|tr -d ' '`;
+EN_DEV=`ip link|grep ': en\|eth'|cut -d':' -f2|tr -d ' '|head -n1`;
+WL_DEV=`ip link|grep ': wl'|cut -d':' -f2|tr -d ' '|head -n1`;
 SWAP_PARTITION=`swapon -s|sed -n 2p|cut -d' ' -f1`;
 HOME_PARTITION=`mount|grep 'on /home'|cut -d' ' -f1`;
 DSK2_PARTITION=`mount|grep 'on /disk2'|cut -d' ' -f1`;
